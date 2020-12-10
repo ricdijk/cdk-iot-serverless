00ca5ec7c5bd8b471e87bd66a9e735593aae0736
Richard@vandijkonline.l <richard.vandijk@futurefacts.nl>
2020-12-10 11:56:07 +0100

    Addid some comment

--- lib/cdk-iot-serverless-stack.ts
+++ lib/cdk-iot-serverless-stack.ts
@@ -1,7 +1,10 @@
 // Environment
 const settings = require('../settings.json') ;;
 
-//global vars
+// region and account for use in
+var arnEnv             = settings.regionName + ":" + settings.accountId;
+
+//All used names declared as global vars (so the same when used multiple times)
 var thingId            = settings.prefix + '-ruuvi'
 var bucketName         = settings.prefix + '-bucket'
 var policyName         = settings.prefix + '-policy'
@@ -13,38 +16,43 @@ var topicRuleName      = settings.prefix + 'rule'
 var topicRuleRoleName  = settings.prefix + 'TopicRuleRole'
 var deleveryStreamName = settings.prefix + 'DeliveryStreamRuuviS3'
 var websiteHandlerName = settings.prefix + '-websiteHandler'
-var arnEnv             = settings.regionName + ":" + settings.accountId;
 
-var fileNameCsr = 'cert/' + thingId  + '-csr.csr';
-var fileNamePublic = 'cert/' + thingId  + '-public.key';
+// Location for the certificate/csr stuff
+var fileNameCsr     = 'cert/' + thingId  + '-csr.csr';
+var fileNamePublic  = 'cert/' + thingId  + '-public.key';
 var fileNamePrivate = 'cert/' + thingId  + '-private.key';
 
 
-//imports
-import * as cdk       from '@aws-cdk/core';
-import * as lambda    from '@aws-cdk/aws-lambda';
-import * as s3        from '@aws-cdk/aws-s3';
-import * as apigw     from '@aws-cdk/aws-apigateway';
-import * as glue      from '@aws-cdk/aws-glue';
-import * as iot       from '@aws-cdk/aws-iot';
-import * as iam       from '@aws-cdk/aws-iam';
-import * as firehose  from '@aws-cdk/aws-kinesisfirehose';
+//imports modules
+import * as cdk            from '@aws-cdk/core';
+import * as lambda         from '@aws-cdk/aws-lambda';
+import * as s3             from '@aws-cdk/aws-s3';
+import * as apigw          from '@aws-cdk/aws-apigateway';
+import * as glue           from '@aws-cdk/aws-glue';
+import * as iot            from '@aws-cdk/aws-iot';
+import * as iam            from '@aws-cdk/aws-iam';
+import * as firehose       from '@aws-cdk/aws-kinesisfirehose';
 import * as secretsmanager from '@aws-cdk/aws-secretsmanager';
 import { PolicyStatement } from "@aws-cdk/aws-iam";
-import { Construct } from 'constructs';
+import { Construct }       from 'constructs';
 
-//stack
+//====================================================================================================================
+// Start of stack
+//====================================================================================================================
 export class CdkStack extends cdk.Stack {
   public readonly rdiCertificateId: cdk.CfnOutput;
 
   //constructor
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+
+// Format output
   console.clear();
   console.log('-- Start -------------------------------------------------------------------------------------------');
 
 
-//-------------- Basics ---------------
+//-------------- S3 creation/retrieving depending on setting ---------------
 var rdiBucket;
 if (settings.newBucket)
 {
@@ -63,17 +71,18 @@ else { //use existing bucet
 
 
 //-------------- Thing ---------------
-	//IotCore - thing
+	//IotCore - create thing
 	const rdiThing = new iot.CfnThing(this, settings.prefix+'thing', {
 		thingName:			thingId,
 		attributePayload:	{}
 	});
 
 
-	// Thing Policy
+	// create Policy for thing
 	const rdiPolicy = new iot.CfnPolicy(this, settings.prefix+'cfnPolicy', {
 		policyName: 	policyName,
 		policyDocument:
+    // used inline policy. Can be stored in file, however is dependend on settings
 		// =========== Start Policy ***************/
 			{
 			  "Version": "2012-10-17",
@@ -111,29 +120,38 @@ else { //use existing bucet
 		// =========== End Policy ***************/
 	});
 
-//===== Certificate =============================================================================================================
+// To connect the IOT device savely to AWS, we need a certificate
+// The way to do this is:
+// 1. cretae key generateKeyPair
+// 2. cretae CSR (Certificate signing request)
+// 3. Sign CSR with private keys
+// 4. Sent signing request to AWS IOT-core
+// 5. retrieve Certificate from AWS (not part of cdk stack, command to retrieve given in logfile)
+// 6. Store private key and certificate on IOT device
+//===== CSR creation =============================================================================================================
+  // Loof on filesytem to see if CSR/key pair is stored?
   const fs = require('fs');
   var csr;
   var keyPrivate:string;
   var keyPublic:string;
 
   if (fs.existsSync(fileNameCsr))
-  {
-    console.log('Read CSR from Storage.')
+  { //if stored, read from file (used sync because I need them right away and have to wait untill We got them)
+    console.log('Read CSR/key from Storage.')
     csr        = fs.readFileSync(fileNameCsr).toString();
     keyPrivate = fs.readFileSync(fileNamePrivate).toString() ;
     keyPublic  = fs.readFileSync(fileNamePublic).toString() ;
   }
   else
-  {
+  { //if not stored, create, sign and store on file
 
-    console.log('generating key')
+    console.log('Generating new CSR/key')
     var forge = require('node-forge');
-    var pki = forge.pki;
+    var pki   = forge.pki;
 
-    // generate a keypair or use one you have already
-    var keys = pki.rsa.generateKeyPair(2048);
-    var csr = forge.pki.createCertificationRequest();
+  // generate a keypair or use one you have already
+    var keys      = pki.rsa.generateKeyPair(2048);
+    var csr       = forge.pki.createCertificationRequest();
     csr.publicKey = keys.publicKey;
     csr.setSubject([{
       name: 'commonName',
@@ -142,10 +160,11 @@ else { //use existing bucet
 
   // sign certification req uest
     csr.sign(keys.privateKey);
-    csr = forge.pki.certificationRequestToPem(csr);
+    csr        = forge.pki.certificationRequestToPem(csr);
     keyPrivate = pki.privateKeyToPem(keys.privateKey)
     keyPublic  = pki.publicKeyToPem(keys.publicKey)
 
+  //store on file (used async, dont need to bother when save is ready)
     fs.writeFile(fileNameCsr, csr,  function(err:any) {
                 if (err) {
                     return console.error('Creation of output file failed: '+ err +'\n\nCSR could not be saved');
@@ -161,24 +180,24 @@ else { //use existing bucet
                     return console.error('Creation of output file failed: '+ err +'\n\nPrivate Key could not be saved');
                 }
             });
-
-// Certificat manager api of cdk can not be used to store certificates, because of security restriction
-
+// Would have preferred to store CSR and keys in SecretManager
+// however SecretManager api of cdk does not have a method to store own secrets because of security restriction (secret will be exposed in cdk code/logging)
+// Therefor choose to store on file
 }
 //==================================================================================================================
 
 
-//==================================================================================================================
+//=== Actual creation of certificate in IOT core ===================================================================
 const rdiCerificate = new iot.CfnCertificate(this, settings.prefix+'Certificate', {
-  status:						    'ACTIVE',
-  certificateMode:			'DEFAULT',
-  certificateSigningRequest:				csr
+  status:						         'ACTIVE',
+  certificateMode:			     'DEFAULT',
+  certificateSigningRequest:	csr
 });
 
 //==================================================================================================================
 
 
-	// bind principle to policy
+	// bind principle/certificate to policy
 	new iot.CfnPolicyPrincipalAttachment(this, settings.prefix+'policyPrincipleAttachment', {
 		policyName: 	policyName,
 		principal:		rdiCerificate.attrArn
@@ -189,11 +208,15 @@ const rdiCerificate = new iot.CfnCertificate(this, settings.prefix+'Certificate'
 		principal:		rdiCerificate.attrArn
 	});
 
+
 //-------------- Topic - FireHose - S3 ---------------
+// ================ define Firehose and add policy to readfrom topic and write to S3 =================
+// ================ Start Policy Statements =================
 	const rdiFirehoseRole = new iam.Role(this, settings.prefix+'FirehoseRole', {
-		assumedBy:		new iam.ServicePrincipal('firehose.amazonaws.com'),
-		roleName:		firehoseName,
+		assumedBy: new iam.ServicePrincipal('firehose.amazonaws.com'),
+		roleName:	  	 firehoseName,
 	});
+  // Policy cretaed with Policy statements (used policy from console as exemple)
 	rdiFirehoseRole.addToPolicy(new PolicyStatement({
         actions: [
             "glue:GetTable",
@@ -273,21 +296,24 @@ const rdiCerificate = new iot.CfnCertificate(this, settings.prefix+'Certificate'
             StringLike:   {"kms:EncryptionContext:aws:kinesis:arn": "arn:aws:kinesis:" + arnEnv + ":stream/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%" }
         }
 	}));
+// ================ End Policy Statements =================
 
-// ================ End STatement =================
-
+  // Create kinesis firehose delivery stream
 	var rdiFirehose = new firehose.CfnDeliveryStream(this, settings.prefix+'DeliveryStream', {
 		deliveryStreamName:		           streamName,
 		deliveryStreamType:		          'DirectPut',
 		s3DestinationConfiguration: {
 			bucketArn:			               rdiBucket.bucketArn,
 			roleArn:			                 rdiFirehoseRole.roleArn,
-			bufferingHints: 	            {intervalInSeconds:300},
+			bufferingHints: 	            {intervalInSeconds:300}, //Hint to write each 300 secs to S3, no restriction on filesize
 			errorOutputPrefix:	          'error',
 			prefix:				                 settings.prefix
 
 		}
 	});
+
+
+  // cretae role for data from topic to deliverystream
 	const rdiTopicRuleRole = new iam.Role(this, settings.prefix+'TopicRuleRole', {
 		assumedBy:		new iam.ServicePrincipal('iot.amazonaws.com'),
 		roleName:		topicRuleRoleName,
@@ -297,6 +323,7 @@ const rdiCerificate = new iot.CfnCertificate(this, settings.prefix+'Certificate'
       actions: ['firehose:PutRecord'],
     }));
 
+  // Create rule to push data to firehose delevery stream
 	new iot.CfnTopicRule(this, settings.prefix+'TopicRule', {
 		ruleName: 		topicRuleName,
 		topicRulePayload:
@@ -318,17 +345,15 @@ const rdiCerificate = new iot.CfnCertificate(this, settings.prefix+'Certificate'
 
 
 
-//-------------- Website ---------------
+//-------------- Website/retrievel of data ---------------
 	// create Athena Database and Table
  	const rdiGlueDatabase = new glue.Database(this, settings.prefix+'IoTDatabase', {
             databaseName: databaseName
     });
-
-
 	const athenaTable = new glue.Table(this, settings.prefix+'IoTDatabaseTable', {
-			database:	rdiGlueDatabase,
+			database:	  rdiGlueDatabase,
 		  tableName: 	tableName,
-			bucket: 	rdiBucket,
+			bucket: 	  rdiBucket,
  		  columns:
 		  [
       		{ name: 'humidity',         			      type: glue.Schema.DOUBLE },
@@ -347,12 +372,10 @@ const rdiCerificate = new iot.CfnCertificate(this, settings.prefix+'Certificate'
     	compressed: false,
     	description: 'Generic IoT data',
     	partitionKeys: [],
-//    	partitionKeys: [{ name: 'timestamp',       			type: glue.Schema.STRING }],
     	s3Prefix: ''
-
     });
 
-    //Lambda for website//Lambda for website
+  //Lambda for website
 	//permissions on S3 (via Athena)
 	const lambdaPolicyS3 = new PolicyStatement()
     lambdaPolicyS3.addActions("s3:GetBucketLocation")
@@ -366,6 +389,7 @@ const rdiCerificate = new iot.CfnCertificate(this, settings.prefix+'Certificate'
     lambdaPolicyS3.addResources(rdiBucket.bucketArn+"*")
     lambdaPolicyS3.addResources("arn:aws:s3:::athena-express-*")   //should be a
 
+  //permissions for lambda on athena/glue
 	const lambdaPolicyAthena = new PolicyStatement()
     lambdaPolicyAthena.addActions("athena:StartQueryExecution")
     lambdaPolicyAthena.addActions("athena:GetQueryExecution")
@@ -381,14 +405,14 @@ const rdiCerificate = new iot.CfnCertificate(this, settings.prefix+'Certificate'
     lambdaPolicyAthena.addResources("*")
 
 
-    //Lambda
+    //Lambda itself
     const rvdWebsite = new lambda.Function(this, settings.prefix+'WebsiteHandler', {
-      runtime: 			lambda.Runtime.NODEJS_12_X,    // execution environment
-      code: 			lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
-      handler: 			'rdiWebsite.website_handler',   // file, function
-      functionName:		websiteHandlerName,
-      timeout:			cdk.Duration.seconds(30),
-      initialPolicy: 	[lambdaPolicyS3, lambdaPolicyAthena ],
+      runtime: 			 lambda.Runtime.NODEJS_12_X,    // execution environment
+      code: 			   lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 			 'rdiWebsite.website_handler',   // file, function
+      functionName:	 websiteHandlerName,
+      timeout:			 cdk.Duration.seconds(30), //AThena is slow, so increased the timeouit to 30 secs
+      initialPolicy: [lambdaPolicyS3, lambdaPolicyAthena ],
       environment: {
       	databaseName: 	databaseName,
       	tableName: 		  tableName,
@@ -397,7 +421,7 @@ const rdiCerificate = new iot.CfnCertificate(this, settings.prefix+'Certificate'
     });
 
 
-    // defines an API Gateway REST API resource backed for website
+    // define an API Gateway REST API resource backed for website
     const rdiGateway = new apigw.LambdaRestApi(this, 'Endpoint', {
         handler: rvdWebsite
     });
@@ -408,25 +432,26 @@ const rdiCerificate = new iot.CfnCertificate(this, settings.prefix+'Certificate'
       value: rdiCerificate.ref
     });
 
-// Thing Url only via cli command. Is Async, cant/dont know how to put it in stack output
+// Output info to console
+// endpoint of thing can be retrive via AWS cli, therfor an shell command
 var exec = require('child_process').exec, child;
 exec('aws iot describe-endpoint --output text --endpoint-type iot:Data-ATS',
     function (error:any, stdout:any, stderr:any) {
         console.log('----------------------------------------------------------------------------------------------------');
-        console.log('Deploying IoT Serverless Stack for:');
+        console.log('Info:');
         console.log('Account:        ' + settings.accountId);
         console.log('Region:         ' + settings.regionName);
         console.log('Prefix:         ' + settings.prefix);
         console.log('New Bucket:     ' + settings.newBucket);
         console.log('Thing Endpoint: ' + stdout.slice(0,-1));
-        console.log('--------------------------------------------------------------------------------------------------------------');
 //        console.log('Private Key (to be used with Ruuvi):\n');
 //        console.log( keyPrivate);
-        console.log('=============================================================================================================================');
+        console.log('==========================================================================================================================================');
+        console.log('Key and Certificate for IOT device can be retrieved:')
         console.log('Private key:    ' + fileNamePrivate);
         console.log('Certificate:    ' + 'aws iot describe-certificate --query "certificateDescription.certificatePem" --output text --certificate-id $CERTIFICATEID');
-        console.log('=============================================================================================================================');
-
+        console.log('== End ===================================================================================================================================');
+        console.log('')
         if (error !== null) {
              console.log('exec error: ' + error);
         }
